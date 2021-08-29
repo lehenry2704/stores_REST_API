ef765459d87922a2eba31b77df84753d381139bc
Henry Le <xuanhao2704@gmail.com>
2021-08-29 20:39:29 +0700

    Fix db.py problems

--- app.py
+++ app.py
@@ -13,10 +13,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'henry'
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 #aapp.config['JWT_AUTH_URL_RULE'] = '/login'
 jwt = JWT(app, authenticate, identity)
 
