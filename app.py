bba414b5963dbe3271cd35db26cb4940c9416c44
Henry Le <xuanhao2704@gmail.com>
2021-08-30 10:44:04 +0700

    Added PostgresSQL environment to app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask
 from flask_restful import Resource, Api
 from flask_jwt import JWT
+import re
 
 from security import authenticate, identity
 from resources.user import UserRegister
@@ -11,8 +12,11 @@ from resources.store import Store, StoreList
 
 app = Flask(__name__)
 
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(uri, 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'henry'
 api = Api(app)
